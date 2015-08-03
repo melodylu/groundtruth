f9a2e8b287488bd47463b8f4b409aee5eb49f7cf
Melody Lu <melodylu@users.noreply.github.com>
2015-08-03 14:24:59 -0700

    Update and rename swap.js to main.js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// global variables
+
+
+
+
+// once the document loads...
+$(document).ready(function() {
+
+    buildHeader();
+    buildMain();
+
+}); // end $(document).ready()
+
+
+
+
+
+
+// _____ helper functions _______
+
+
+function buildHeader() {
+
+}
+
+function buildMain() {
+
+}
+
